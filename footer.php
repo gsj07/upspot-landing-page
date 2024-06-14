88378fdef879acf52737b6d6e154896a1b8eb16d
Gaurav Jethuri <115738279+gsj07@users.noreply.github.com>
2024-06-14 16:51:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+<div class="footerdiv">
+<div class="logodiv">
+    <img class="weblogo" src="<?php bloginfo('template_directory')?>/images/mainlogo.png">
+    <h1 class="logoh1">UpSpot</h1>
+</div>
+
+<div class="navdiv">
+   
+         <li class="li">About</li>
+         <li class="li">Contact</li>
+         <li class="li">Blog</li>
+         <li class="li">Terms</li>
+         <li class="li">Privacy</li>
+
+  
+</div>
+
+<div class="sociallinksdiv">
+    <div>
+   <img class="footimg" src="<?php bloginfo('template_directory')?>/images/sociallogo1.png">
+</div>
+
+<div>
+   <img class="footimg" src="<?php bloginfo('template_directory')?>/images/sociallogo2.png">
+</div>
+
+<div>
+   <img class="footimg" src="<?php bloginfo('template_directory')?>/images/sociallogo3.png">
+</div>
+   
+</div>
+
+</div>
+
+ 
+</body>
+</html>
