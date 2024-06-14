e355aef52da72a05885b2ad3c332641275e4e44a
Gaurav Jethuri <115738279+gsj07@users.noreply.github.com>
2024-06-14 12:13:26 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
+</head>
+<body>
+    
+
+<div class="headerdiv">
+<div class="logodiv">
+    <img class="weblogo" src="<?php bloginfo('template_directory')?>/images/logo1.png">
+    <h1 class="logoh1">UpSpot</h1>
+</div>
+
+<div class="navdiv">
+   
+         <li class="li">How it works</li>
+         <li class="li">Costing</li>
+         <li class="li">Our Talents</li>
+         <li class="li">Testimonials</li>
+<li class="li">Login</li>
+
+  
+</div>
+
+<div class="btndiv">
+    <button class="topbtn">
+    Get Started
+</button>
+</div>
+
+</div>
